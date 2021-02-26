4af5c7ffc640e23488bdf671adfcba1e1aca585d
Sebastian Montoya <seb246@gmail.com>
2021-02-25 21:31:42 -0500

    practice 2

--- 2-25/index.js
+++ 2-25/index.js
@@ -23,3 +23,32 @@ function sumMix(x) {
 function sumMix(x) {
   return x.map((a) => +a).reduce((a, b) => a + b);
 }
+
+// task 2:
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+// my solution:
+
+var countSheep = function (num) {
+  //your code here
+  let phrase = "";
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      phrase += i + " sheep...";
+    }
+  }
+  return phrase;
+};
+
+// recommended solution:
+
+var countSheep = function (num) {
+  let str = "";
+  for (let i = 1; i <= num; i++) {
+    str += `${i} sheep...`;
+  }
+  return str;
+};
